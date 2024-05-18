9567fd12c599f1b06d2ae266a0e41fa8f8b2dbec
Ilyas022 <ilya.shchetko@mail.ru>
2024-05-18 15:34:48 +0300

    refactor: move link list from navbar to separate file and map them

--- src/components/Header/Navbar/index.tsx
+++ src/components/Header/Navbar/index.tsx
@@ -1,19 +1,14 @@
-import {
-	BANK_PAGE_ROUTE,
-	CONTACTS_PAGE_ROUTE,
-	HOME_PAGE_ROUTE,
-	TIMELINE_PAGE_ROUTE,
-} from 'constants/routes'
-
+import { links } from './config'
 import { NavbarComp, Navlink } from './styled'
 
 function Navbar() {
 	return (
 		<NavbarComp>
-			<Navlink to={HOME_PAGE_ROUTE}>Home</Navlink>
-			<Navlink to={TIMELINE_PAGE_ROUTE}>Timeline</Navlink>
-			<Navlink to={BANK_PAGE_ROUTE}>Bank card</Navlink>
-			<Navlink to={CONTACTS_PAGE_ROUTE}>Contaсts</Navlink>
+			{links.map(({ path, title }) => (
+				<Navlink key={path} to={path}>
+					{title}
+				</Navlink>
+			))}
 		</NavbarComp>
 	)
 }
